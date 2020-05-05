522fb743857e79e6e27601661676a4836c9d0d7c
Surindra <sb989@njit.edu>
2020-05-05 01:07:11 -0400

    db_changes

--- create_user_db.sql
+++ create_user_db.sql
@@ -4,8 +4,9 @@ USE message_system;
 
 CREATE TABLE user_info(
 
-	Username VARCHAR(40) NOT NULL,
-	Pword	VARCHAR(40) NOT NULL,
+	Username VARCHAR(512) NOT NULL,
+	Pword	BLOB NOT NULL,
+    Salt BLOB NOT NULL,
 	LoggedIn VARCHAR(8) NOT NULL,
 	PublicKey VARCHAR(40),
     index(Username),
