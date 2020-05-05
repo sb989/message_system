de5860c69d6db8cadd830ddce8ecaecbf4299813
sb989 <sb989@njit.edu>
2020-05-05 01:08:21 -0400

    server

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
-file = open('key.txt',r)
+file = open('key.txt','r')
 privateKey = file.read()
 privateKey = privateKey.encode()
 file.close()
